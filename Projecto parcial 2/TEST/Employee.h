f485dfccf7a0eefa84162181b3a7deb57701589e
LautiLezama <lautarolezama@hotmail.com>
2019-11-28 07:13:54 -0300

    imrdy2.0

---
+++ Projecto parcial 2/TEST/Employee.h
@@ -0,0 +1,44 @@
+#ifndef employee_H_INCLUDED
+#define employee_H_INCLUDED
+#include "LinkedList.h"
+#include <string.h>
+#include <stdio.h>
+#include <conio.h>
+typedef struct
+{
+    int idLlamada;
+    char fecha[50];
+    int numeroCliente;
+    int idProblema;
+    char solucionado[50];
+}Employee;
+
+Employee* employee_new();
+Employee* employee_newParametros(char*,char*,char*,char*,char*);
+void employee_delete();
+
+int setIdLlamada(Employee* this,int int1);
+int setNumeroCliente(Employee* this,int int1);
+int setIdProblema(Employee* this,int int1);
+int setFecha(Employee* this,char* char1);
+int setSolucion(Employee* this,char* char1);
+
+int getIdLlamada(Employee* this,int* int1);
+int getNumeroCliente(Employee* this,int* int1);
+int getIdProblema(Employee* this,int* int1);
+int getFecha(Employee* this,char* char1);
+int getSolucion(Employee* this,char* char1);
+
+int filtrarProblema1(void*);
+int filtrarProblema2(void*);
+int filtrarProblema3(void*);
+int filtrarProblema4(void*);
+int filtrarProblema5(void*);
+
+int compararChars(void* , void* );
+int compararInts(void*,void*);
+
+int employee_findById(Employee*,LinkedList*);
+void employee_showOneEmployee(LinkedList*,int);
+
+#endif // employee_H_INCLUDED
---
+++ Projecto parcial 2/TEST/parser.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile, LinkedList* pArrayListEmployee)
+{
+    Employee* listEmployees;
+    int r = 0;
+    char idLlamada[50];
+    char fecha[50];
+    char numeroCliente[50];
+    char idProblema[50];
+    char solucionado[50];
+    if(pFile == NULL)
+    {
+        return -1;
+    }
+    fscanf(pFile,"%[^,],%[^,],%[^,],%[^,],%[^\n]\n",idLlamada,fecha,numeroCliente,idProblema,solucionado);
+    do
+    {
+        fscanf(pFile,"%[^,],%[^,],%[^,],%[^,],%[^\n]\n",idLlamada,fecha,numeroCliente,idProblema,solucionado);
+        listEmployees = employee_newParametros(idLlamada,fecha,numeroCliente,idProblema,solucionado);
+        ll_add(pArrayListEmployee, listEmployees);
+
+    }
+    while(!feof(pFile));
+
+    return r;
+}
+
+
+
+
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+/*int parser_EmployeeFromBinary(FILE* pFile, LinkedList* pArrayListEmployee)
+{
+
+    Employee* listEmployees;
+    if(pFile == NULL)
+    {
+        return -1;
+    }
+    while(!feof(pFile))
+    {
+        listEmployees = employee_new();
+        fread(listEmployees,sizeof(Employee),1,pFile);
+        if(feof(pFile))
+        {
+            break;
+        }
+        ll_add(pArrayListEmployee, listEmployees);
+    }
+
+    return 1;
+}*/
